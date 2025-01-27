08f4cf66c1a2815efe4ba3eea0cb4557cf16cd1d
jbishop6 <jenniferjbshp@gmail.com>
2025-01-26 20:05:43 -0700

    Add files via upload

--- mat_to_txt.m
+++ mat_to_txt.m
@@ -1,59 +1,3 @@
-% % Jennifer Bishop
-% % 1/26/25
-% % The intention of this script is to convert .mat files to .txt files. 
-% 
-% clc
-% clear
-% close all
-% 
-% % Loading file
-% 
-% data = load('3311_surface.mat');
-% 
-% % Inspecting data
-% 
-% % disp(data)
-% 
-% fv = data.fv;
-% % disp(data.fv)
-% 
-% vertices = fv.vertices;
-% faces = fv.faces;
-% 
-% numVertices = size(vertices,1); % Number of vertices
-% indices = (1:numVertices)'; % Creating indices
-% 
-% % Using applicable data to make same format _ref.txt file
-% 
-% % values = vertices(:,1);
-% 
-% for i = 1:numVertices
-%     connected_faces = find(any(faces == i, 2));
-%     values(i) = connected_faces(1);
-% end
-% 
-% refData = [indices, values'];
-% 
-% 
-% % size(vertices)  % Should be  [n,3]
-% % size(faces) % Should be [m,3] or [m,4]
-% 
-% % Writing to txt file
-% 
-% output_file = '3311_surface.txt';
-% 
-% fileID = fopen(output_file,'w');
-% 
-% fprintf(fileID,'%6d %6d\n', refData');
-% fclose(fileID)
-% 
-% % Assuming 'fv' is the structure containing faces and vertices
-% trisurf(fv.faces, fv.vertices(:,1), fv.vertices(:,2), fv.vertices(:,3));
-% axis equal;
-
-
-
-
 % Jennifer Bishop
 % 1/26/25
 % The intention of this script is to convert .mat files to .txt files. 
@@ -63,41 +7,48 @@ clear
 close all
 
 % Loading file
-data = load('4022_surface.mat');
+
+data = load('3311_surface.mat');
 
 % Inspecting data
+
+% disp(data)
+
 fv = data.fv;
+% disp(data.fv)
+
 vertices = fv.vertices;
 faces = fv.faces;
 
 numVertices = size(vertices,1); % Number of vertices
-indices = (1:numVertices)'; % Create indices for vertices
+indices = (1:numVertices)'; % Creating indices
 
-% Initialize values array
-values = zeros(numVertices, 1);
+% Using applicable data to make same format _ref.txt file
 
-% Loop through each vertex and assign corresponding connected face reference
+% values = vertices(:,1);
+
 for i = 1:numVertices
-    % Find all faces that include vertex i
-    connected_faces = find(any(faces == i, 2));  
-    
-    % If the vertex has at least one connected face, take the first connected face index
-    if ~isempty(connected_faces)
-        values(i) = connected_faces(1);  % Use the first connected face index
-    else
-        values(i) = 0;  % If no connection, use 0 or some placeholder
-    end
+    connected_faces = find(any(faces == i, 2));
+    values(i) = connected_faces(1);
 end
 
-% Combine indices and values to create the reference data
-refData = [indices, values];
+refData = [indices, values'];
 
-% Writing to .txt file
-output_file = '4022_surface_ref.txt';
-fileID = fopen(output_file, 'w');
-fprintf(fileID, '%6d %6d\n', refData');
-fclose(fileID);
 
-% Optional: Visualize the surface mesh
+% size(vertices)  % Should be  [n,3]
+% size(faces) % Should be [m,3] or [m,4]
+
+% Writing to txt file
+
+output_file = '3311_surface.txt';
+
+fileID = fopen(output_file,'w');
+
+fprintf(fileID,'%6d %6d\n', refData');
+fclose(fileID)
+
+% Assuming 'fv' is the structure containing faces and vertices
 trisurf(fv.faces, fv.vertices(:,1), fv.vertices(:,2), fv.vertices(:,3));
 axis equal;
+
+
